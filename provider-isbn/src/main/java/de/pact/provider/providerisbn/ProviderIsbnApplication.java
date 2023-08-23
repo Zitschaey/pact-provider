81763b5a311b611f12350b5bac7466a7ec8e2b0d
Zitschaey <cj.vance@outlook.de>
2023-08-23 18:04:01 +0200

    init commit

---
+++ provider-isbn/src/main/java/de/pact/provider/providerisbn/ProviderIsbnApplication.java
@@ -0,0 +1,13 @@
+package de.pact.provider.providerisbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderIsbnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderIsbnApplication.class, args);
+	}
+
+}
---
+++ provider-isbn/src/test/java/de/pact/provider/providerisbn/ProviderIsbnApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pact.provider.providerisbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderIsbnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
